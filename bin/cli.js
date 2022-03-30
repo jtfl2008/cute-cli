a0e92c92aa81f36564509a8f1150247fcb3c158c
dongjianglong <dongjianglong@csftgroup.com>
2022-03-30 12:52:37 +0800

    feat✨:  完成仓库选择，标签选择，仓库下载，依赖自动下载等功能

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,39 @@
 #! /usr/bin/env node
 
-const program = require('commander');
+const program = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
 
 program
   .command('create <app-name>')
   .description('create a new project powered by cute-cli')
   .option('-f,--force', 'overwrite target directory if it exist')
   .action((name, options) => {
-    console.log('name:', name, 'options:', options);
-  });
+    require('../lib/create.js')(name, options)
+  })
 
 program
   .version(`v${require('../package.json').version}`)
-  .usage('<command> [options]');
+  .usage('<command> [options]')
 
-program.parse(process.argv);
+program.on('--help', () => {
+  // 使用 figlet 绘制 Logo
+  console.log(
+    '\r\n' +
+      figlet.textSync('cute-cli', {
+        font: 'Dancing Font',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true
+      })
+  )
+
+  // 新增说明信息
+  console.log(
+    `\r\nRun ${chalk.cyan(
+      `cute-cli <command> --help`
+    )} for detailed usage of given command\r\n`
+  )
+})
+program.parse(process.argv)
---
+++ readme.md
@@ -0,0 +1,14 @@
+# cute-cli
+
+## 实现了从仓库选择，版本选择，仓库下载，依赖下载等
+
+### 使用依赖包
+
+- commander 命令行自定义指令
+- inquirer 命令行询问用户问题，记录回答结果
+- chalk 控制台输出内容样式美化 ora 控制台
+- loading 样式 figlet 控制台打印 logo
+- download-git-repo 下载远程模版
+- fs-extra 系统 fs 模块的扩展，提供了更多便利的 API，并继承了 fs 模块的 API
+- shellljs 命令行工具，方便执行脚本
+- figlet 生成基于 ASCII 的艺术字
